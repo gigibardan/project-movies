4c32c2f391db9cdaf5a4154bfb1fd7a209de69f7
gigibardan <gheorghe.bardan@gmail.com>
2026-08-18 18:57:31 +0300

    chore: cleanup bolt.new residuals, unused deps and components

--- next.config.js
+++ next.config.js
@@ -3,7 +3,15 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: { unoptimized: true },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'image.tmdb.org',
+        pathname: '/t/p/**',
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
